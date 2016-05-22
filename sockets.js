65275b05aadb356d15e275a60ab83b5997aa39ff
Emilio Srougo <Emilios1995@gmail.com>
2016-05-21 22:52:19 -0500

    turn

--- sockets.js
+++ sockets.js
@@ -107,20 +107,7 @@ module.exports = function (server, config) {
         var credentials = [];
         // allow selectively vending turn credentials based on origin.
         var origin = client.handshake.headers.origin;
-        if (!config.turnorigins || config.turnorigins.indexOf(origin) !== -1) {
-            config.turnservers.forEach(function (server) {
-                var hmac = crypto.createHmac('sha1', server.secret);
-                // default to 86400 seconds timeout unless specified
-                var username = Math.floor(new Date().getTime() / 1000) + (server.expiry || 86400) + "";
-                hmac.update(username);
-                credentials.push({
-                    username: username,
-                    credential: hmac.digest('base64'),
-                    urls: server.urls || server.url
-                });
-            });
-        }
-        client.emit('turnservers', credentials);
+        client.emit('turnservers', config.turnservers);
     });
 
 
